9783fe92b1a4a410ac4ec39879e02630fd98b673
Natan Gabriel Almeida de castro <natsuaz27@gmail.com>
2023-02-19 20:27:45 -0300

    adiciona responcividade para laptop e tablet

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 // Rotatividade automatica
 let count = 1;
-let label = document.querySelectorAll(".bar");
-console.log(label);
+let label = document.querySelectorAll("label.bar");
 setInterval(function () {
   nextImage();
 }, 5000);
 
 function nextImage() {
   count++;
-  if (count >= 5) {
+
+  if (count > 5) {
     count = 1;
   }
   label.forEach((label) => label.classList.remove("active"));
   document.querySelector(`[for=slide${count}]`).classList.add("active");
-
   document.getElementById("slide" + count).checked = true;
 }
-document.getElementById("slide");
 
 let countValue = document.querySelectorAll("[data-count]");
 
@@ -30,3 +28,9 @@ function atualizarValor(e) {
   document.querySelector(`[for=slide${count}]`).classList.add("active");
 }
 // fim da rotatividade automatica
+
+let categorias = document.querySelector(".categories")
+
+function toggleMenu() {
+  categorias.classList.toggle("open")
+}
